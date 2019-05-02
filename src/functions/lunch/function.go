163b3ffd59fb4ecadea86e00ca86d126e268d4f1
Shuhei Kagaya <shuhei.lab@gmail.com>
2019-05-02 20:37:30 +0900

    add slack slash command function.

---
+++ src/functions/lunch/function.go
@@ -0,0 +1,45 @@
+package lunch
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+func Lunch(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		e := "Method Not Allowd."
+		log.Println(e)
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte(e))
+		return
+	}
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println("ReadAllError: %v\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	parsed, err := url.ParseQuery(string(b))
+	if err != nil {
+		log.Printf("ParseQueryError: %v\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	if parsed.Get("token") != os.Getenv("SLACK_TOKEN") {
+		e := "Unauthorized Token."
+		log.Printf(e)
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte(e))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
